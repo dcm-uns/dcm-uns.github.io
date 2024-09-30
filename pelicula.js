ad612dfaa035d2a1293fc7c7ad7e8525ef88b017
dcm@uns <38795567+dcm-uns@users.noreply.github.com>
2024-09-30 11:29:07 -0300

    un json

---
+++ pelicula.js
@@ -0,0 +1,7 @@
+{
+    "titulo":"Star Wars: The Rise of Skywalker",
+    "imagenfondo": "/jn52me8AagfNt7r84SgQbV0R9ZG.jpg",
+    "resumen": "The surviving Resistance faces the First Order once again as the journey of Rey, Finn and Poe Dameron continues. With the power and knowledge of generations behind them, the final battle begins.",
+    "precio": 12,
+    "personas": "Homero, Lisa y Bart"
+}
