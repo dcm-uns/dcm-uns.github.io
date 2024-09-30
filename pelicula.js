bfa4af52256dc17c1bbf4652e88fdde946f62507
dcm@uns <38795567+dcm-uns@users.noreply.github.com>
2024-09-30 11:41:11 -0300

    Update pelicula.js

--- pelicula.js
+++ pelicula.js
@@ -1,6 +1,6 @@
 {
     "titulo":"Star Wars: The Rise of Skywalker",
-    "imagenfondo": "/jn52me8AagfNt7r84SgQbV0R9ZG.jpg",
+    "imagenfondo": "https://image.tmdb.org/t/p/original/jn52me8AagfNt7r84SgQbV0R9ZG.jpg",
     "resumen": "The surviving Resistance faces the First Order once again as the journey of Rey, Finn and Poe Dameron continues. With the power and knowledge of generations behind them, the final battle begins.",
     "precio": 12,
     "personas": "Homero, Lisa y Bart"
